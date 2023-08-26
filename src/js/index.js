719b9374af55a78f7e1803560ce9dba5c332cf28
SnowFox-95 <321mts95@gmail.com>
2023-08-26 20:21:35 +0300

    CountryForm in working

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,16 @@
-import mobileNav from './modules/mobile-nav.js';
-mobileNav();
+import autoComplete from "@tarekraafat/autocomplete.js";
+import countryList from "./helpers/countryList";
+
+const autoCompleteJS = new autoComplete({
+  selector: "#autocompleteCountry",
+  placeHolder: "e.g Bali, Indonesia",
+  data: {
+    src: [countryList],
+  },
+  resultItem: {
+    highlight: true,
+  },
+});
+
+//import mobileNav from './modules/mobile-nav.js';
+//mobileNav();
